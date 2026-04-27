0366118dd0ac07a63233669690fb8a7c56d09e55
Janiel Senador <154493279+Janiel-Senador@users.noreply.github.com>
2026-04-27 12:50:44 +0800

    Add files via upload

---
+++ CCA/Models/CommissionRate.cs
@@ -0,0 +1,6 @@
+﻿public class CommissionRate
+{
+    public string Title { get; set; } // e.g., "Digital Bust"
+    public string Description { get; set; }
+    public decimal Price { get; set; }
+}
