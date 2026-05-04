93f276cf5c3b3f7864201d69d9164b7707bcb280
FeveBaclayon <lanzraijin@gmail.com>
2026-05-04 16:06:23 +0800

    Add files via upload
    -add front-end MVC views and fix layout rendering issues
    -adding Browse, Commission and Artist Page

---
+++ CCA/Controllers/ArtistsController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CCA.Controllers
+{
+    public class ArtistsController : Controller
+    {
+        // GET: /Artists/Browse - Marketplace page
+        public IActionResult Browse()
+        {
+            return View();
+        }
+
+        // GET: /Artist/{slug} - Artist Profile page
+        public IActionResult Profile(string slug)
+        {
+            // For now, just return the same profile view for any slug
+            return View();
+        }
+    }
+}
---
+++ CCA/Controllers/CommissionsController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CCA.Controllers
+{
+    public class CommissionsController : Controller
+    {
+        // GET: /Commissions/Create - Commission Request Form
+        public IActionResult Create(string artistSlug)
+        {
+            // Pass artist name to view for display (front-end only)
+            ViewBag.ArtistName = !string.IsNullOrEmpty(artistSlug) ? "Evelyn Vance" : "Select an Artist";
+            return View();
+        }
+
+        // POST: /Commissions/Create - Just redirects for demo
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create()
+        {
+            // Front-end demo: just redirect to a "success" message
+            return RedirectToAction("Confirmation");
+        }
+
+        // GET: /Commissions/Confirmation - Simple success page
+        public IActionResult Confirmation()
+        {
+            return View();
+        }
+    }
+}
